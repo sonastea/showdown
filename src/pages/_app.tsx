38cf6203b5ecf0e51b530002f28ceca150775128
sonastea <sonastea@gmail.com>
2022-06-16 22:22:52 -0700

    Update _app baseurl and prisma 3.15.1->x.xx.2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,7 +17,7 @@ function getBaseUrl() {
   if (typeof window === "undefined") return ""; // Browser should use current path
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
 
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+  return `http://${window.location.hostname}:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 }
 
 export default withTRPC<AppRouter>({
