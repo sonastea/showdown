2ed6df13d21c5c997e8c5b59cbcb887214f35212
sonastea <sonastea@gmail.com>
2023-05-01 20:02:16 -0700

    feat: add `react-dropzone` & change cloudinary_cloud_name env

--- src/components/UploadForm.tsx
+++ src/components/UploadForm.tsx
@@ -1,6 +1,7 @@
 import imageCompression, { Options } from "browser-image-compression";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
 import AlertSuccess from "./AlertSuccess";
 
 const url = "https://api.cloudinary.com/v1_1/k-showdown/auto/upload";
@@ -10,6 +11,40 @@ const UploadForm: React.FC<{ toggleActive: Function }> = ({ toggleActive }) => {
   const [image, setImage] = useState<File | null>();
   const [success, setSuccess] = useState<boolean>(false);
   const [isUploading, setIsUploading] = useState<boolean>(false);
+
+  const handleFileInput = useCallback((file: File[]) => {
+    setImage(file[0]);
+    const compressOptions: Options = { maxSizeMB: 3, useWebWorker: true };
+    try {
+      const compressFile = async () => {
+        await imageCompression(file[0], compressOptions).then(
+          (compressedFile) => {
+            setImage(compressedFile);
+          }
+        );
+      };
+      compressFile();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const validator = (file: File) => {
+    if (file && file.length >= 2) {
+      (document.getElementById("filesInput") as HTMLInputElement).value = "";
+      return {
+        code: "too-many-files",
+        message: "You can upload only 1 image at a time",
+      };
+    }
+    return null;
+  };
+
+  const { getRootProps, getInputProps, isDragActive, open } = useDropzone({
+    maxFiles: 1,
+    onDrop: handleFileInput,
+    validator,
+  });
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -65,58 +100,53 @@ const UploadForm: React.FC<{ toggleActive: Function }> = ({ toggleActive }) => {
     (document.getElementById("uploadForm") as HTMLFormElement).reset();
   };
 
-  const handleFileInput = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files.length >= 2) {
-      alert("You can upload only 1 image at a time");
-      (document.getElementById("filesInput") as HTMLInputElement).value = "";
-      return;
-    }
-    if (!e.target.files) return;
-
-    const imageFile = e.target.files[0];
-    const compressOptions: Options = { maxSizeMB: 3, useWebWorker: true };
-
-    try {
-      const compressedFile = await imageCompression(imageFile, compressOptions);
-      setImage(compressedFile);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <div
       className="z-10 absolute flex justify-center w-full min-h-screen backdrop-blur"
       onClick={() => toggleActive(false)}
     >
       <div
-        onClick={(e: React.MouseEvent<HTMLDivElement>) => e.stopPropagation()}
-        className="bg-slate-500 absolute rounded shadow-xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 inset-x-auto p-2"
+        {...getRootProps({
+          className:
+            "text-mina-50 bg-slate-500 absolute rounded shadow-xl w-3/4 md:w-1/2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 inset-x-auto p-2",
+          onClick: (e) => e.stopPropagation(),
+        })}
       >
         <form
-          className="flex flex-wrap sm:p-8 sm:justify-items-end place-content-center"
+          className="flex flex-col flex-wrap p-2 sm:p-8 sm:justify-items-end place-content-center items-center sm:whitespace-nowrap overflow-hidden"
           onSubmit={handleSubmit}
           id="uploadForm"
           encType="multipart/form-data"
         >
           <input
-            className="text-white"
-            id="filesInput"
-            type="file"
-            name="image"
-            onChange={handleFileInput}
-            multiple
+            {...getInputProps({
+              id: "filesInput",
+              type: "file",
+              name: "image",
+            })}
           />
-          <input
+          {isDragActive ? (
+            <p className="p-2 cursor-pointer" onClick={open}>
+              Drop the files here ...
+            </p>
+          ) : (
+            <p className="p-2 cursor-pointer" onClick={open}>
+              Drag &apos;n&apos; drop file here, or{" "}
+              <span className="font-bold text-mina-200">click</span> to select
+              files
+            </p>
+          )}
+          <button
             className={`${
               !image
                 ? "cursor-not-allowed bg-mina/70 text-mina-900"
                 : "cursor-pointer hover:bg-mina-400"
-            } rounded bg-mina p-1 m-4 sm:m-1 font-bold justify-self-center sm:justify-self-end text-mina-900 w-min`}
+            } rounded bg-mina p-1 m-4 sm:m-2 font-bold justify-self-center sm:justify-self-end text-mina-900 w-min`}
             type="submit"
-            value="Upload"
             disabled={!image}
-          />
+          >
+            Upload
+          </button>
         </form>
         {isUploading && (
           <div className="grid">
@@ -130,7 +160,6 @@ const UploadForm: React.FC<{ toggleActive: Function }> = ({ toggleActive }) => {
           </div>
         )}
         <AlertSuccess onSuccess={success} toggleActive={setSuccess} />
-
         <div className="flex flex-wrap justify-center">
           {image && (
             <div className="m-1 object-scale-down">
