5a4af423dff46e085f061209ff24aaf1e63d76ff
sonastea <sonastea@gmail.com>
2023-08-18 20:28:36 -0700

    refactor:  in its own file

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -5,8 +5,9 @@ import React, { useState } from "react";
 import { trpc } from "src/utils/trpc";
 
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "./api/auth/[...nextauth]";
 import { useRouter } from "next/router";
+import MemeListingSkeleton from "src/components/MemeListingSkeleton";
+import { authOptions } from "./api/auth/[...nextauth]";
 
 export async function getServerSideProps(context: any) {
   const session = await getServerSession(context.req, context.res, authOptions);
@@ -129,19 +130,6 @@ const Admin: NextPage = () => {
         </div>
       </div>
     </>
-  );
-};
-
-const MemeListingSkeleton = () => {
-  return (
-    <li className="flex flex-wrap justify-around animate-pulse p-2">
-      <div className="text-gray-200 self-center m-2 blur-sm w-24">0</div>
-      <div
-        className="bg-gray-200"
-        style={{ width: "96px", height: "96px" }}
-      ></div>
-      <button className="w-12 bg-gray-200 h-8 px-1 text-sm rounded-lg self-center m-2 disabled:cursor-not-allowed disabled:bg-mina-400/80 disabled:line-through"></button>
-    </li>
   );
 };
 
