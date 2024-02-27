eec3bf90e22263dda0d0d05f9d7c4ce00cba3f0f
sonastea <sonastea@gmail.com>
2024-02-27 00:21:56 -0800

    feat: resolve deprecated drizzle type & planetscale client

--- src/lib/drizzle.ts
+++ src/lib/drizzle.ts
@@ -1,15 +1,14 @@
-import { connect } from "@planetscale/database";
-import { InferModel } from "drizzle-orm";
+import { Client } from "@planetscale/database";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { meme, vote } from "drizzle/schema";
 
-const connection = connect({
+const planetscaleClient = new Client({
   host: process.env["DATABASE_HOST"],
   username: process.env["DATABASE_USERNAME"],
   password: process.env["DATABASE_PASSWORD"],
 });
 
-export type Meme = InferModel<typeof meme>;
-export type Vote = InferModel<typeof vote>;
+export type Meme = typeof meme.$inferInsert;
+export type Vote = typeof vote.$inferInsert;
 
-export const db = drizzle(connection);
+export const db = drizzle(planetscaleClient);
