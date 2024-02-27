39b421f6514ffcdef70ceb3ae09bffb4b945c3e7
sonastea <sonastea@gmail.com>
2024-02-27 02:54:15 -0800

    fix: getting ip address for rate limit middleware

--- src/lib/rate-limiter.ts
+++ src/lib/rate-limiter.ts
@@ -1,6 +1,8 @@
-import rateLimit from "express-rate-limit";
+import rateLimit, { RateLimitRequestHandler } from "express-rate-limit";
 
-export const rateLimiter = rateLimit({
+export const rateLimiter: RateLimitRequestHandler = rateLimit({
+  keyGenerator: (req) =>
+    req.ip || req.headers["x-forwarded-for"] || req.headers["x-real-ip"] || req.connection.remoteAddress,
   windowMs: 5 * 60 * 1000, // 5 minutes
   max: 50, // Limit each IP to 50 requests per `window` (here, per 5 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
