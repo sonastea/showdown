d4a08a692e78adbe37a2664aa8042ddc50134edf
sonastea <sonastea@gmail.com>
2025-03-14 10:35:55 -0700

    chore: use bun & cleanup dependencies
    chore: bump to v0.4.0

--- migration.ts
+++ migration.ts
@@ -1,9 +1,6 @@
-import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
-
-config();
 
 const dbMigration = async () => {
   const connectionString = process.env.DATABASE_URL;
